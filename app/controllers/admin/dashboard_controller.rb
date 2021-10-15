2edb3ffd9ac2d8068b00049885b29ddff27bd98c
NehaSijaria <nehasijaria@gmail.com>
2021-10-15 16:33:40 -0400

    add categories to admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with name: ENV['username'], password: ENV['password']
   def show
     @product_count = Product.all.count
     @category_count = Category.all.count
